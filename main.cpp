879a1768dd24de83d2e2b8c442456710b3c6703d
moldovexc <alexanderparpulansky@gmail.com>
2023-12-05 17:53:53 +0200

    url parsing for http and ftp

--- main.cpp
+++ main.cpp
@@ -1,223 +1,70 @@
 #include <iostream>
-#include <cctype>
 #include <fstream>
+#include <sstream>
 #include <string>
 
-enum TSymbolType {
-    protocol, domain, port, path, colon, period, quotas, othersy, semicolon, machine
+struct URLComponents {
+    std::string scheme;
+    std::string user;
+    std::string password;
+    std::string host;
+    std::string port;
+    std::string path;
 };
 
-char Char;
-TSymbolType Symbol;
-std::string Spelling;
+void parseURL(const std::string& urlString) {
+    URLComponents components;
 
-std::ifstream inputFile("/Users/moldovexc/CLionProjects/parserurl/input.txt");
+    size_t schemePos = urlString.find("://");
+    if (schemePos != std::string::npos) {
+        components.scheme = urlString.substr(0, schemePos);
+        size_t hostStartPos = schemePos + 3;
 
-void GetNextChar() {
-    if (!inputFile.is_open()) {
-        std::cerr << "Error opening the file!" << std::endl;
-        return;
-    }
-    Char = inputFile.get();
-}
+        size_t userEndPos = urlString.find(':', hostStartPos);
+        size_t hostEndPos = urlString.find('@', hostStartPos);
+        size_t portStartPos = urlString.find(':', hostStartPos);
+        size_t pathStartPos = urlString.find('/', schemePos + 3);
 
-void error(const char* message) {
-    fprintf(stderr, "Error: %s\n", message);
-}
-
-void GetNextSymbol() {
-    while (Char != '\0' && !isalpha(Char) && Char != ':' && Char != '.' && Char != '/' && Char != ';' && Char != '\"') {
-        GetNextChar();
-    }
-
-    if (Char == '\0' || inputFile.eof()) {
-        Symbol = othersy;
-        std::cout << "End of Input" << std::endl;
-        return;
-    }
-
-    while (Char == ' ') {
-        GetNextChar();
-    }
-
-    switch (toupper(Char)) {
-        // Inside the switch statement where the domain is identified
-        case 'A' ... 'Z': {
-            Spelling = "";
-            while (isalpha(Char) || isdigit(Char) || Char == '.') {
-                Spelling.push_back(Char);
-                GetNextChar();
-            }
-
-            if (Spelling == "http" || Spelling == "https") {
-                Symbol = protocol;
-                std::cout << "Protocol: " << Spelling << std::endl;
-            } else {
-                Symbol = domain;
-                std::string domainStr = Spelling;
-
-                // Check if the domain starts with "www."
-                if (domainStr.substr(0, 4) == "www.") {
-                    domainStr = domainStr.substr(4); // Remove "www."
-                }
-
-                // Extract and output the domain
-                std::cout << "Domain: " << domainStr << std::endl;
-            }
-        } break;
-
-
-
-        case '0' ... '9': {
-            Spelling = "";
-            while (isdigit(Char)) {
-                Spelling.push_back(Char);
-                GetNextChar();
-            }
-
-            Symbol = port;
-            std::cout << "Port: " << Spelling << std::endl;
-        } break;
-        case '/' : {
-            Symbol = path;
-            Spelling = "";
-            while (Char != ';' && Char != '\"' && Char != '\n' && Char != EOF) {
-                Spelling.push_back(Char);
-                GetNextChar();
-            }
-            std::string pathStr = Spelling;
-
-            // Check if "www." exists in the path and remove it
-            size_t wwwPos = pathStr.find("www.");
-            if (wwwPos != std::string::npos) {
-                pathStr.erase(wwwPos, 4); // Remove "www."
-            }
-
-//            std::cout << "Path: " << pathStr << std::endl;
-            std::cout<<pathStr<<std::endl;
-        } break;
-
-        case 'w' : {
-            Spelling.push_back(Char);
-            GetNextChar();
-            if(Char == 'w') {
-                GetNextChar();
-                Spelling.push_back(Char);
-                if(Char == 'w') {
-                    Symbol = machine;
-                    GetNextChar();
-                    Spelling.push_back(Char);
-                    std::cout << "Machine: " << Spelling << std::endl;
-                }
-            }
-        }
-        case ':' : {
-            Symbol = colon;
-            std::cout << "Colon" << std::endl;
-
-            while (Char == ':' || Char == ' ') {
-                GetNextChar();
-            }
-
-            if (Char == '/') {
-                GetNextChar();
-                Symbol = path;
-                Spelling = "";
-
-                std::string removedPart = ""; // Variable to store the removed part
-
-                while (Char != ';' && Char != '\"' && Char != '\n' && Char != EOF) {
-                    Spelling.push_back(Char);
-                    GetNextChar();
-                }
-
-                size_t wwwPos = Spelling.find("www.");
-                if (wwwPos != std::string::npos) {
-                    removedPart = "www"; // Store the removed part
-                    Spelling.erase(wwwPos, 4);
-                    Symbol = machine; // Update the symbol to represent the removal of "www"
-                }
-
-                size_t www1Pos = Spelling.find("www1.");
-                if (www1Pos != std::string::npos) {
-                    removedPart = "www1"; // Store the removed part
-                    Spelling.erase(www1Pos, 5);
-                    Symbol = machine; // Update the symbol to represent the removal of "www1"
-                }
-
-                std::cout << "Path: " << Spelling << std::endl;
-
-                if (!removedPart.empty()) {
-                    std::cout << "Machine: " << removedPart << std::endl; // Print the removed part
-                }
-            }
-        } break;
-
-
-
-        case ';' : {
-            Symbol = semicolon;
-            std::cout << "Semicolon" << std::endl;
-
-            while (Char != '\n' && Char != EOF) {
-                GetNextChar();
-            }
-        } break;
-        case '\"' : {
-            Symbol = quotas;
-            Spelling = "";
-            while (Char != '\"' && Char != '\n' && Char != EOF) {
-                Spelling.push_back(Char);
-                GetNextChar();
-            }
-            std::cout << "String: " << Spelling << std::endl;
-        } break;
-        default: {
-            Symbol = othersy;
-            GetNextChar();
-            std::cout << "Unknown Symbol" << std::endl;
-        } break;
-    }
-}
-
-int accept(TSymbolType symbol) {
-    if (Symbol == symbol) {
-        GetNextSymbol();
-        return 1;
-    }
-    return 0;
-}
-
-void Field() {
-    while (!accept(protocol) && !accept(port) && !accept(path) && !accept(colon) && !accept(semicolon)) {
-        if (Symbol == othersy) {
-            error("field: unexpected symbol");
-            return;
+        if (components.scheme != "http" && components.scheme != "https" && userEndPos != std::string::npos && (hostEndPos == std::string::npos || userEndPos < hostEndPos)) {
+            components.user = urlString.substr(hostStartPos, userEndPos - hostStartPos);
+            components.password = urlString.substr(userEndPos + 1, hostEndPos - userEndPos - 1);
+            components.host = urlString.substr(hostEndPos + 1, (portStartPos != std::string::npos ? portStartPos : pathStartPos) - hostEndPos - 1);
         } else {
-            GetNextSymbol();
+            components.host = urlString.substr(hostStartPos, (portStartPos != std::string::npos ? portStartPos : pathStartPos) - hostStartPos);
+        }
+
+        if (portStartPos != std::string::npos && portStartPos < pathStartPos) {
+            components.port = urlString.substr(portStartPos + 1, pathStartPos - portStartPos - 1);
+        }
+
+        if (pathStartPos != std::string::npos) {
+            components.path = urlString.substr(pathStartPos);
         }
     }
-}
 
-void Record() {
-    Field();
-    while (Symbol == period) {
-        accept(period);
-        Field();
+    std::cout << "Scheme: " << components.scheme << std::endl;
+    if (components.scheme != "http" && components.scheme != "https") {
+        std::cout << "User: " << components.user << std::endl;
+        std::cout << "Password: " << components.password << std::endl;
     }
-}
-
-void DataFile() {
-    Record();
-    while (Symbol != othersy && !inputFile.eof()) {
-        Record();
-    }
-    inputFile.close();
+    std::cout << "Host: " << components.host << std::endl;
+    std::cout << "Port: " << (components.port.empty() ? "default" : components.port) << std::endl;
+    std::cout << "Path: " << components.path << std::endl;
 }
 
 int main() {
-    GetNextChar();
-    GetNextSymbol();
-    DataFile();
+    std::ifstream file("/Users/moldovexc/CLionProjects/parserurl/input.txt");
+    if (!file.is_open()) {
+        std::cout << "Unable to open file!" << std::endl;
+        return 1;
+    }
+
+    std::string line;
+    while (std::getline(file, line)) {
+        parseURL(line);
+        std::cout << "-----------------------------" << std::endl;
+    }
+
+    file.close();
     return 0;
 }
